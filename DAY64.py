6fa8154f57a4d68b93667786a5776b0459e32242
patrick0117 <48704453+patrick0117@users.noreply.github.com>
2020-02-17 16:40:18 +0800

    Create DAY64.py

---
+++ DAY64.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 16:34:16 2020
+  Hidden layer      test loss
+1.     44             0.018
+2.     26             0.021
+3.     8              0.013
+4.     53             0.014
+
+@author: tribology2020
+"""
+
