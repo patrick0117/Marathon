086e753d2322431851c96b0b0563e255c6114631
patrick0117 <48704453+patrick0117@users.noreply.github.com>
2020-02-18 15:34:12 +0800

    Create DAY65.py

---
+++ DAY65.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 14:54:28 2020
+    batch size   learning rate  Activation  Regularization  test loss
+1.     19             0.3          RELU          None         0.119
+2.       5            0.03         RELU          None         0.036
+@author: tribology2020
+"""
+
