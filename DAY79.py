e0c5c169d8e222b538ef03099f4ad009848668cd
patrick0117 <48704453+patrick0117@users.noreply.github.com>
2020-03-10 19:49:20 +0800

    Create DAY79.py

---
+++ DAY79.py
@@ -0,0 +1,121 @@
+import os
+import keras
+
+# 本範例不需使用 GPU, 將 GPU 設定為 "無"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+# 從 Keras 的內建功能中，取得 train 與 test 資料集
+train, test = keras.datasets.cifar10.load_data()
+
+## 資料前處理 normalization
+def preproc_x(x, flatten=True):
+    x = x / 255.
+    if flatten:
+        x = x.reshape((len(x), -1))
+    return x
+
+# separating into ten classes
+def preproc_y(y, num_classes=10):
+    if y.shape[-1] == 1:
+        y = keras.utils.to_categorical(y, num_classes)
+    return y  
+
+x_train, y_train = train
+x_test, y_test = test
+
+# 資料前處理 - X 標準化
+x_train = preproc_x(x_train)
+x_test = preproc_x(x_test)
+
+# 資料前處理 -Y 轉成 onehot
+y_train = preproc_y(y_train)
+y_test = preproc_y(y_test)
+
+def build_mlp(input_shape, output_units=10, num_neurons=[512, 256, 128]):
+    input_layer = keras.layers.Input(input_shape)
+    
+    for i, n_units in enumerate(num_neurons):
+        if i == 0:
+            x = keras.layers.Dense(units=n_units, activation="relu", name="hidden_layer"+str(i+1))(input_layer)
+        else:
+            x = keras.layers.Dense(units=n_units, activation="relu", name="hidden_layer"+str(i+1))(x)
+    
+    out = keras.layers.Dense(units=output_units, activation="softmax", name="output")(x)
+    
+    model = keras.models.Model(inputs=[input_layer], outputs=[out])
+    return model
+
+## 超參數設定
+LEARNING_RATE = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
+EPOCHS = 50
+BATCH_SIZE = 256
+MOMENTUM = 0.95
+
+results = {}
+"""
+使用迴圈，建立不同 Learning rate 的模型並訓練
+"""
+for lr in LEARNING_RATE:
+    keras.backend.clear_session() # 把舊的 Graph 清掉
+    print("Experiment with LR = %.6f" % (lr))
+    model = build_mlp(input_shape=x_train.shape[1:])
+    model.summary()
+    optimizer = keras.optimizers.SGD(lr=lr, nesterov=True, momentum=MOMENTUM)
+    model.compile(loss="categorical_crossentropy", metrics=["accuracy"], optimizer=optimizer)
+
+    model.fit(x_train, y_train, 
+              epochs=EPOCHS, 
+              batch_size=BATCH_SIZE, 
+              validation_data=(x_test, y_test), 
+              shuffle=True)
+    
+    # Collect results
+    train_loss = model.history.history["loss"]
+    valid_loss = model.history.history["val_loss"]
+    train_acc = model.history.history["acc"]
+    valid_acc = model.history.history["val_acc"]
+    
+    exp_name_tag = "exp-lr-%s" % str(lr)
+    results[exp_name_tag] = {'train-loss': train_loss,
+                             'valid-loss': valid_loss,
+                             'train-acc': train_acc,
+                             'valid-acc': valid_acc}
+    
+import matplotlib.pyplot as plt
+
+color_bar = ["r", "g", "b", "y", "m", "k"]
+
+plt.figure(figsize=(8,6))
+for i, cond in enumerate(results.keys()):
+    plt.plot(range(len(results[cond]['train-loss'])),results[cond]['train-loss'], '-', label=cond, color=color_bar[i])
+    plt.plot(range(len(results[cond]['valid-loss'])),results[cond]['valid-loss'], '--', label=cond, color=color_bar[i])
+plt.title("Loss")
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(8,6))
+for i, cond in enumerate(results.keys()):
+    plt.plot(range(len(results[cond]['train-acc'])),results[cond]['train-acc'], '-', label=cond, color=color_bar[i])
+    plt.plot(range(len(results[cond]['valid-acc'])),results[cond]['valid-acc'], '--', label=cond, color=color_bar[i])
+plt.title("Accuracy")
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
