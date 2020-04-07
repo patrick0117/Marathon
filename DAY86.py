af4931a026a92e2cf61ffcdedea7b291622ee89a
patrick0117 <48704453+patrick0117@users.noreply.github.com>
2020-04-07 16:14:35 +0800

    86

---
+++ DAY86.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 16:09:37 2020
+
+@author: 88697
+"""
+
+import os
+import keras
+
+# 本範例不需使用 GPU, 將 GPU 設定為 "無"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+train, test = keras.datasets.cifar10.load_data()
+
+## 資料前處理
+def preproc_x(x, flatten=True):
+    x = x / 255.
+    if flatten:
+        x = x.reshape((len(x), -1))
+    return x
+
+def preproc_y(y, num_classes=10):
+    if y.shape[-1] == 1:
+        y = keras.utils.to_categorical(y, num_classes)
+    return y    
+
+x_train, y_train = train
+x_test, y_test = test
+
+# 資料前處理 - X 標準化
+x_train = preproc_x(x_train)
+x_test = preproc_x(x_test)
+
+# 資料前處理 -Y 轉成 onehot
+y_train = preproc_y(y_train)
+y_test = preproc_y(y_test)
+
+from keras.layers import BatchNormalization
+
+"""
+建立神經網路，並加入 BN layer
+"""
+def build_mlp(input_shape, output_units=10, num_neurons=[512, 256, 128]):
+    input_layer = keras.layers.Input(input_shape)
+    
+    for i, n_units in enumerate(num_neurons):
+        if i == 0:
+            x = keras.layers.Dense(units=n_units, 
+                                   activation="relu", 
+                                   name="hidden_layer"+str(i+1))(input_layer)
+            x = BatchNormalization()(x)
+        else:
+            x = keras.layers.Dense(units=n_units, 
+                                   activation="relu", 
+                                   name="hidden_layer"+str(i+1))(x)
+            x = BatchNormalization()(x)
+    
+    out = keras.layers.Dense(units=output_units, activation="softmax", name="output")(x)
+    
+    model = keras.models.Model(inputs=[input_layer], outputs=[out])
+    return model
+
+## 超參數設定
+LEARNING_RATE = 1e-3
+EPOCHS = 50
+BATCH_SIZE = 1024
+MOMENTUM = 0.95
+
+"""
+# 載入 Callbacks, 並將監控目標設為 validation loss, 且只存最佳參數時的模型
+"""
+from keras.callbacks import ModelCheckpoint
+
+model_ckpt = ModelCheckpoint(filepath="./tmp.h5", 
+                             monitor="val_loss", 
+                             save_best_only=True)
+
+model = build_mlp(input_shape=x_train.shape[1:])
+model.summary()
+optimizer = keras.optimizers.SGD(lr=LEARNING_RATE, nesterov=True, momentum=MOMENTUM)
+model.compile(loss="categorical_crossentropy", metrics=["accuracy"], optimizer=optimizer)
+
+model.fit(x_train, y_train, 
+          epochs=EPOCHS, 
+          batch_size=BATCH_SIZE, 
+          validation_data=(x_test, y_test), 
+          shuffle=True,
+          callbacks=[model_ckpt]
+         )
+
+# Collect results
+train_loss = model.history.history["loss"]
+valid_loss = model.history.history["val_loss"]
+train_acc = model.history.history["accuracy"]
+valid_acc = model.history.history["accuracy"]
+
+# Load back
+model = keras.models.load_model("./tmp.h5")
+loss_loadback, acc_loadback = model.evaluate(x_test, y_test)
+import matplotlib.pyplot as plt
+
+plt.plot(range(len(train_loss)), train_loss, label="train loss")
+plt.plot(range(len(valid_loss)), valid_loss, label="valid loss")
+plt.hlines(y=loss_loadback, xmin=0, xmax=len(train_loss), colors='r', linestyles='--')
+plt.legend()
+plt.title("Loss")
+plt.show()
+
+plt.plot(range(len(train_acc)), train_acc, label="train accuracy")
+plt.plot(range(len(valid_acc)), valid_acc, label="valid accuracy")
+plt.hlines(y=acc_loadback, xmin=0, xmax=len(train_loss), colors='r', linestyles='--')
+plt.legend()
+plt.title("Accuracy")
+plt.show()
+
+
+
+
+
+
+
+
+
+
