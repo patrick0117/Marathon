4540115ef207624ae5d08d981afdb0b15233e9a6
patrick0117 <48704453+patrick0117@users.noreply.github.com>
2020-04-16 20:31:07 +0800

    Create DAY90.py

---
+++ DAY90.py
@@ -0,0 +1,48 @@
+import os
+import keras
+os.environ["CUDA_VISIBLE_DEVICES"] = "" # 使用 CPU
+
+import cv2 # 載入 cv2 套件
+import matplotlib.pyplot as plt
+
+train, test = keras.datasets.cifar10.load_data()
+
+image = train[0][0] # 讀取圖片
+
+plt.imshow(image)
+plt.show()
+
+# 把彩色的圖片轉為灰度圖
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+plt.imshow(gray)
+plt.show()
+
+# 調用 cv2.calcHist 函數，回傳值就是 histogram
+# cv2.calcHist(images, channels, mask, histSize, ranges)   
+hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+plt.figure()
+plt.title("Grayscale Histogram")
+plt.xlabel("Bins")
+plt.ylabel("# of Pixels")
+plt.plot(hist)
+plt.xlim([0, 256])
+plt.show()
+
+print("hist shape:", hist.shape, "\n直方圖中前兩個值:", hist[:2]) # 1 表示該灰度圖中，只有 1 個 pixel 的值是 0，0 個 pixel 的值是 1
+
+chans = cv2.split(image) # 把圖像的 3 個 channel 切分出來
+colors = ("r", "g", "b")
+plt.figure()
+plt.title("'Flattened' Color Histogram")
+plt.xlabel("Bins")
+plt.ylabel("# of Pixels")
+
+# 對於所有 channel
+for (chan, color) in zip(chans, colors):
+    # 計算該 channel 的直方圖
+    hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+ 
+    # 畫出該 channel 的直方圖
+    plt.plot(hist, color = color)
+    plt.xlim([0, 256])
+plt.show()
